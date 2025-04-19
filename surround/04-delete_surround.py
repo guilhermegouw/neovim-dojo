9ae57326cb01c23fcd42623b24f40844ab9a7a8a
guilhermegouw <guilherme.gouw@gmail.com>
2025-04-18 22:11:08 -0300

    Added a delete surround

---
+++ surround/04-delete_surround.py
@@ -0,0 +1,12 @@
+"""
+There are repeating surrounds in lot's of places at the code.
+Can you fix it?
+"""
+
+my_set = {{{"Guilherme", "Luciana", "Pedro"}}}
+my_list = [[["Guilherme", "Luciana", "Pedro"]]]
+my_tuple = ((("Guilherme", "Luciana", "Pedro")))
+
+print(my_set)
+print(my_list)
+print(my_tuple)
