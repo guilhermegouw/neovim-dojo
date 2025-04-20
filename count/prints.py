ada472a8d12d5bb2b739f7edaa0921b9323e9a8d
guilhermegouw <guilherme.gouw@gmail.com>
2025-04-20 17:37:55 -0300

    add count

---
+++ count/prints.py
@@ -0,0 +1,19 @@
+"""
+We need to print guilherme's data.
+Expected
+
+Name: Guilherme
+Last Name: Gouw
+Country: Brazil
+City: Sao Paulo
+"""
+
+
+guilherme = {
+        "Name": "Guilherme",
+        "Last Name": "Gouw",
+        "Country": "Brazil",
+        "City": "Sao Paulo"
+        }
+
+print(f"Name: {guilherme['Name']}")
