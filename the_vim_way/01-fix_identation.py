24558afbc8ffabac2e42df922cda863e90c28612
guilhermegouw <guilherme.gouw@gmail.com>
2025-04-20 08:37:21 -0300

    fix spaces

--- the_vim_way/01-fix_identation.py
+++ the_vim_way/01-fix_identation.py
@@ -2,11 +2,13 @@
 There are some identation errors here.
 Please fix it.
 """
+
+
 def if_loop_wrong_ident(array):
     for item in array:
     print(item)
     if item == "right word":
-        if len(item) == 10:
-        print(item)
+    if len(item) == 10:
+    print(item)
     else:
         print("Something is wrong here")
